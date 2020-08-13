edfebc6cdaf748a07be58dd461a4721889a67aef
7onetella <7onetella@gmail.com>
2020-08-13 01:34:08 -0400

    applying codemods

--- ui/tests/integration/components/passwords/delete-test.js
+++ ui/tests/integration/components/passwords/delete-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | passwords/delete', function(hooks) {
 
     await render(hbs`{{passwords/delete}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | passwords/delete', function(hooks) {
       {{/passwords/delete}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
--- ui/tests/integration/components/passwords/edit-test.js
+++ ui/tests/integration/components/passwords/edit-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | passwords/edit', function(hooks) {
 
     await render(hbs`{{passwords/edit}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | passwords/edit', function(hooks) {
       {{/passwords/edit}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
