d56432a2dc11578957fe6e88f80f31344e090d01
7onetella <7onetella@gmail.com>
2020-11-18 02:39:49 -0500

    moving auth event handler to its own file

---
+++ api/internal/handlers/authevent.go
@@ -0,0 +1,102 @@
+package handlers
+
+import (
+	"time"
+
+	. "github.com/7onetella/users/api/internal/dbutil"
+	. "github.com/7onetella/users/api/internal/httputil"
+	. "github.com/7onetella/users/api/internal/model"
+	"github.com/7onetella/users/api/pkg/crypto"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthEventHandler struct {
+	RequestHandler RequestHandler
+	UserService    UserService
+}
+
+func (ah AuthEventHandler) Context() *gin.Context {
+	return ah.RequestHandler.Context
+}
+
+func (ah AuthEventHandler) DenyAccessForAnonymous(category Category, reason Reason) {
+	c := ah.Context()
+	e := New(category, reason)
+	c.AbortWithStatusJSON(401, e)
+	ah.RecordEvent("", e.ErrorCodeHuman)
+}
+
+func (ah AuthEventHandler) DenyAccessForUser(userID string, category Category, reason Reason) {
+	c := ah.Context()
+	e := New(category, reason)
+	c.AbortWithStatusJSON(401, e)
+	ah.RecordEvent(userID, e.ErrorCodeHuman)
+}
+
+func (ah AuthEventHandler) ExtractEventID(s string) (string, error) {
+	decoded, err := crypto.Base64Decode(s)
+	if err != nil {
+		return "", err
+	}
+	return decoded, nil
+}
+
+func (ah AuthEventHandler) IsSigninSessionStillValid(timestamp int64, limit time.Duration) bool {
+	return (time.Now().Unix() - timestamp) > int64(limit.Seconds())
+}
+
+func (ah AuthEventHandler) RecordEvent(userID, eventName string) {
+	rh := ah.RequestHandler
+	event := rh.NewAuthEvent(userID, eventName)
+	dberr := ah.UserService.RecordAuthEvent(event)
+	if dberr != nil {
+		LogErr(rh.TX(), "db error", dberr.Err)
+	}
+}
+
+func (ah AuthEventHandler) AccessDeniedMissingData(userID string, category Category, reason Reason) {
+	rh := ah.RequestHandler
+	c := rh.Context
+	userService := ah.UserService
+
+	e := New(category, reason)
+	event := rh.NewAuthEvent(userID, e.ErrorCodeHuman)
+	userService.RecordAuthEvent(event)
+
+	mde := MissingDataError{
+		Error:              *e,
+		SigninSessionToken: crypto.Base64Encode(event.ID),
+	}
+
+	c.AbortWithStatusJSON(422, mde)
+}
+
+func (ah AuthEventHandler) FinishSecondAuth(userID, eventName, message string) {
+	rh := ah.RequestHandler
+	c := rh.Context
+	userService := ah.UserService
+
+	event := rh.NewAuthEvent(userID, eventName)
+	userService.RecordAuthEvent(event)
+
+	secEvent := rh.NewAuthEvent(userID, "sec_auth_generated")
+	userService.RecordAuthEvent(secEvent)
+
+	c.AbortWithStatusJSON(401, gin.H{
+		"reason":                 event.Event,
+		"message":                message,
+		"signin_session_token":   crypto.Base64Encode(event.ID),
+		"webauthn_session_token": crypto.Base64Encode(secEvent.ID),
+	})
+}
+
+func (ah AuthEventHandler) IsWebAuthnSessionTokenValidForUer(userID, webAuthnSessionToken string) bool {
+	userService := ah.UserService
+	eventID, _ := crypto.Base64Decode(webAuthnSessionToken)
+	user, dberr := userService.FindUserByAuthEventID(eventID)
+	if dberr != nil {
+		return false
+	}
+
+	return userID == user.ID
+}
--- api/internal/handlers/user.go
+++ api/internal/handlers/user.go
@@ -10,7 +10,6 @@ import (
 	. "github.com/7onetella/users/api/internal/httputil"
 	. "github.com/7onetella/users/api/internal/jsonutil"
 	. "github.com/7onetella/users/api/internal/model"
-	"github.com/7onetella/users/api/pkg/crypto"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/mfcochauxlaberge/jsonapi"
@@ -84,117 +83,6 @@ func Signup(userService UserService) gin.HandlerFunc {
 		}
 		c.String(http.StatusOK, out)
 	}
-}
-
-type AuthEventHandler struct {
-	RequestHandler RequestHandler
-	UserService    UserService
-}
-
-func (ah AuthEventHandler) Handle(err error, code int, userID string, category Category, reason Reason) bool {
-	if err != nil {
-		rh := ah.RequestHandler
-		c := rh.Context
-		userService := ah.UserService
-
-		e := New(category, reason)
-		c.AbortWithStatusJSON(code, e)
-
-		event := rh.NewAuthEvent(userID, e.Message)
-
-		dberr := userService.RecordAuthEvent(event)
-		if dberr != nil {
-			LogErr(rh.TX(), "db error", dberr.Err)
-		}
-		return true
-	}
-	return false
-}
-
-func (ah AuthEventHandler) Context() *gin.Context {
-	return ah.RequestHandler.Context
-}
-
-func (ah AuthEventHandler) DenyAccessForAnonymous(category Category, reason Reason) {
-	c := ah.Context()
-	e := New(category, reason)
-	c.AbortWithStatusJSON(401, e)
-	ah.RecordEvent("", e.ErrorCodeHuman)
-}
-
-func (ah AuthEventHandler) DenyAccessForUser(userID string, category Category, reason Reason) {
-	c := ah.Context()
-	e := New(category, reason)
-	c.AbortWithStatusJSON(401, e)
-	ah.RecordEvent(userID, e.ErrorCodeHuman)
-}
-
-func (ah AuthEventHandler) ExtractEventID(s string) (string, error) {
-	decoded, err := crypto.Base64Decode(s)
-	if err != nil {
-		return "", err
-	}
-	return decoded, nil
-}
-
-func (ah AuthEventHandler) IsSigninSessionStillValid(timestamp int64, limit time.Duration) bool {
-	return (time.Now().Unix() - timestamp) > int64(limit.Seconds())
-}
-
-func (ah AuthEventHandler) RecordEvent(userID, eventName string) {
-	rh := ah.RequestHandler
-	event := rh.NewAuthEvent(userID, eventName)
-	dberr := ah.UserService.RecordAuthEvent(event)
-	if dberr != nil {
-		LogErr(rh.TX(), "db error", dberr.Err)
-	}
-}
-
-func (ah AuthEventHandler) AccessDeniedMissingData(userID string, category Category, reason Reason) {
-	rh := ah.RequestHandler
-	c := rh.Context
-	userService := ah.UserService
-
-	e := New(category, reason)
-	event := rh.NewAuthEvent(userID, e.ErrorCodeHuman)
-	userService.RecordAuthEvent(event)
-
-	mde := MissingDataError{
-		Error:              *e,
-		SigninSessionToken: crypto.Base64Encode(event.ID),
-	}
-
-	c.AbortWithStatusJSON(422, mde)
-}
-
-func (ah AuthEventHandler) FinishSecondAuth(userID, eventName, message string) {
-	rh := ah.RequestHandler
-	c := rh.Context
-	userService := ah.UserService
-
-	event := rh.NewAuthEvent(userID, eventName)
-	userService.RecordAuthEvent(event)
-
-	secEvent := rh.NewAuthEvent(userID, "sec_auth_generated")
-	userService.RecordAuthEvent(secEvent)
-
-	c.AbortWithStatusJSON(401, gin.H{
-		"reason":                 event.Event,
-		"message":                message,
-		"signin_session_token":   crypto.Base64Encode(event.ID),
-		"webauthn_session_token": crypto.Base64Encode(secEvent.ID),
-	})
-}
-
-func (ah AuthEventHandler) IsWebAuthnSessionTokenValidForUer(userID, webAuthnSessionToken string) bool {
-	userService := ah.UserService
-	eventID, _ := crypto.Base64Decode(webAuthnSessionToken)
-	user, dberr := userService.FindUserByAuthEventID(eventID)
-	if dberr != nil {
-		return false
-	}
-
-	return userID == user.ID
 }
 
 // swagger:operation POST /signin signin
@@ -351,7 +239,7 @@ func Signin(userService UserService, ttl time.Duration, issuer string) gin.Handl
 		}
 
 		if cred.IsUsernamePresent() {
-			userFromDB, dberr := auth.UserService.FindByEmail(cred.Username)
+			userFromDB, dberr := userService.FindByEmail(cred.Username)
 			if dberr != nil {
 				auth.DenyAccessForAnonymous(DatabaseError, QueryingFailed)
 				return
